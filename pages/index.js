2c0b0ec0310883fbd397ddd872fc15c8952c521c
Edwin obando <edwin.alex1995@gmail.com>
2020-02-09 16:32:58 -0500

    test 1

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,8 @@
 import React, { Component } from "react";
-import Router from "next/router";
+import LandingPage from "./landing";
 
 export default class Index extends Component {
-  componentDidMount = () => {
-    Router.push("/components");
-  };
-
   render() {
-    return <div />;
+    return <LandingPage />;
   }
 }
