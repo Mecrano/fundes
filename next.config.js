7cc7f1acf4da7501be21b9240daf7333ee2e493a
Edwin Obando <eobando@datacommerce.io>
2020-02-18 09:28:21 -0500

    deploy with domain conected

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ const webpack = require("webpack");
 const path = require("path");
 
 module.exports = withPlugins([[withSass], [withImages]], {
-  assetPrefix: './fundes',
   webpack(config, options) {
     config.resolve.modules.push(path.resolve("./"));
     return config;
